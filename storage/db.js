d3cef5b27327aafcc02881799791d9677f33ce39
Ethan Hsu <ethanhsu128@gmail.com>
2022-03-05 23:34:43 -0500

    configure database

--- storage/db.js
+++ storage/db.js
@@ -1,7 +1,18 @@
 const { Pool } = require("pg");
 
-// connection information stored in .env file
-const pool = new Pool();
+const devConfig = {
+  user: process.env.PGUSER,
+  password: process.env.PGHOST,
+  host: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+};
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
