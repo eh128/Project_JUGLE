843e29656eadf13e643d8a68eadbc4a6324ac767
Ethan Hsu <ethanhsu128@gmail.com>
2022-03-06 00:41:42 -0500

    production

--- storage/db.js
+++ storage/db.js
@@ -1,18 +1,26 @@
 const { Pool } = require("pg");
 
-const devConfig = {
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
-};
-const proConfig = {
-  connectionString: process.env.DATABASE_URL,
-};
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+// const devConfig = {
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   port: process.env.PGPORT,
+// };
+// const proConfig = {
+//   connectionString: process.env.DATABASE_URL,
+// };
+// const pool = new Pool(
+//   process.env.NODE_ENV === "production" ? proConfig : devConfig
+// );
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
